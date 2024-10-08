5bad1be1787efda46cfcd9768f05ce40d56702e1
= <potlavani9194@gmail.com>
2024-10-08 21:09:40 +0530

    Functional Component

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// Functional Component
+// function Greet(){
+//     return <h1>Hello Vani!</h1>
+// }
+
+// Arrow Functions
+// const Greet =() => <h1>This is Functional Component!</h1>
+
+// Exporting Component
+// export default Greet
+
+// exporting directly as constant ..so we ne3ed to use braces while importing  
+export const Greet =() => <h1>This is Functional Component!</h1>
