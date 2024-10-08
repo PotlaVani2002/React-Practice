8c1e8bb23c6e7ac1671836e45aee25f08faaad50
= <potlavani9194@gmail.com>
2024-10-08 21:17:00 +0530

    Class Component is added

---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        This is Class Component!
+      </div>
+    )
+  }
+}
+
+export default Welcome
