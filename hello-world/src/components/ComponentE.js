e3bd556add8b9dc7f9ba4d40f329ec06884dbaab
= <potlavani9194@gmail.com>
2024-10-15 22:54:36 +0530

    contexttype property is using for getting context value

--- hello-world/src/components/ComponentE.js
+++ hello-world/src/components/ComponentE.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 export class ComponentE extends Component {
   render() {
     return (
       <div>
+       Componet E {this.context}
         <ComponentF />
       </div>
     )
   }
 }
-
+ComponentE.contextType=UserContext
 export default ComponentE
