1affdaf22da20acc2036fae41ebea4ee81987eca
= <potlavani9194@gmail.com>
2024-10-15 22:53:07 +0530

    Context API

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -11,22 +11,18 @@ import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounterTwo from './components/HoverCounterTwo';
 import User from './components/User';
 import Counter from './components/Counter';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/userContext';
 function App() {
   return (
     <div className="App">
-       {/* <ClickCounterTwo />
-       <HoverCounterTwo />
-       <User render={(isLoggedIn)=>isLoggedIn? "Vani": "Guest"} /> */}
-       <Counter render={(count, incrementCount) => (
-          <ClickCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      />
-      
-      <Counter
-        render={(count, incrementCount) => (
-          <HoverCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      />
+    {/* Sending default Value */}
+    {/* <ComponentC/> */}
+
+    {/* Passing a value */}
+     <UserProvider value="Vani">
+       <ComponentC />
+      </UserProvider>
     </div>
   );
 }
